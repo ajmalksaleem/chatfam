ceca022f269f6431eb840a1f3f7a4291c916726d
ajmal k saleem <ajmalksaleem@gmail.com>
2024-10-29 20:17:03 +0530

    chnages in index.js

--- backend/index.js
+++ backend/index.js
@@ -17,56 +17,6 @@ app.use(cookieParser())
 const __dirname = path.resolve()
 
 connectDb()
-   const server = app.listen(3000, () => {
-      console.log("server started running  .!");
-    });
-
-    const io = new Server(server, {
-      pingTimeout: 60000, 
-      cors: {
-        origin: 'http://localhost:5173',
-        // credentials: true, 
-      },
-    });
-
-    io.on('connection', (socket)=>{
-      console.log('connected to socket.io')
-
-      socket.on("setup", (userData)=>{
-        socket.join(userData._id)
-        console.log(userData._id)
-        socket.emit("connected")
-      })
-      
-      socket.on("join chat", (room)=>{
-        socket.join(room)
-        console.log("user joined room"+room)
-      })
-
-      socket.on("typing", (room) => socket.in(room).emit("typing"));
-      socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-      socket.on("new message",(newMessageReceived)=>{
-        try {
-          const chat = newMessageReceived.chat;
-          if (!chat.users) {
-            console.log('Chat users not defined');
-            return;
-          }
-          chat.users.forEach((user) => {
-            if (user._id == newMessageReceived.sender._id) return;
-            socket.in(user._id).emit("message received", newMessageReceived);
-          });
-        } catch (error) {    
-          console.error('New message error:', error);
-        }
-      })
-      socket.off("setup", (userData)=>{
-       console.log("USER DISCONNECTED")
-        socket.leave(userData._id)
-      })
-    })
-
 
   app.use("/api/user", userRoutes);
   app.use('/api/chat', chatRoutes)
@@ -87,4 +37,54 @@ connectDb()
       message,
     });
   });
+
+  const server = app.listen(3000, () => {
+    console.log("server started running  .!");
+  });
+
+  const io = new Server(server, {
+    pingTimeout: 60000, 
+    cors: {
+      origin: 'http://localhost:5173',
+      // credentials: true, 
+    },
+  });
+
+  io.on('connection', (socket)=>{
+    console.log('connected to socket.io')
+
+    socket.on("setup", (userData)=>{
+      socket.join(userData._id)
+      console.log(userData._id)
+      socket.emit("connected")
+    })
+    
+    socket.on("join chat", (room)=>{
+      socket.join(room)
+      console.log("user joined room"+room)
+    })
+
+    socket.on("typing", (room) => socket.in(room).emit("typing"));
+    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+    socket.on("new message",(newMessageReceived)=>{
+      try {
+        const chat = newMessageReceived.chat;
+        if (!chat.users) {
+          console.log('Chat users not defined');
+          return;
+        }
+        chat.users.forEach((user) => {
+          if (user._id == newMessageReceived.sender._id) return;
+          socket.in(user._id).emit("message received", newMessageReceived);
+        });
+      } catch (error) {    
+        console.error('New message error:', error);
+      }
+    })
+    socket.off("setup", (userData)=>{
+     console.log("USER DISCONNECTED")
+      socket.leave(userData._id)
+    })
+  })
   
